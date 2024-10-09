bb035cc1128a4aa9b0ebf614fb0fd461f62c4cf4
AdminL3 <l-blu@outlook.de>
2024-10-09 11:27:23 +0200

    fixed output path

--- Excel Liga.py
+++ Excel Liga.py
@@ -1,29 +1,30 @@
 import csv
 
-with open('data/Mannschaft/Namen.txt', 'r', encoding='latin-1') as file:
+with open('data/Liga/Namen.txt', 'r', encoding='latin-1') as file:
     namen = file.read().splitlines()
 
-with open('data/Mannschaft/Ordnungszahl.txt', 'r', encoding='latin-1') as file:
+with open('data/Liga/Ordnungszahl.txt', 'r', encoding='latin-1') as file:
     ordnungszahl = file.read().splitlines()
 
-with open('data/Mannschaft/Altersklasse.txt', 'r', encoding='latin-1') as file:
+with open('data/Liga/Altersklasse.txt', 'r', encoding='latin-1') as file:
     altersklasse = file.read().splitlines()
 
-with open('data/Mannschaft/Geschlecht.txt', 'r', encoding='latin-1') as file:
+with open('data/Liga/Geschlecht.txt', 'r', encoding='latin-1') as file:
     geschlecht = file.read().splitlines()
 
-with open('data/Mannschaft/Mannschaft_ID.txt', 'r', encoding='latin-1') as file:
-    mannschafts_ID = file.read().splitlines()
+with open('data/Liga/Liganummer.txt', 'r', encoding='latin-1') as file:
+    liganummer = file.read().splitlines()
 
-with open('data/Mannschaft/Widget_ID.txt', 'r', encoding='latin-1') as file:
+with open('data/Liga/Widget_ID.txt', 'r', encoding='latin-1') as file:
     widget_ID = file.read().splitlines()
 
 
 # Read the content of the code_snippets.txt file
-with open('data/code_snippets_big.txt', 'r', encoding='utf-8') as file:
+with open('data/Liga/code_snippets_big.txt', 'r', encoding='utf-8') as file:
     code_snippets_big = file.read()
-with open('data/code_snippets_small.txt', 'r', encoding='utf-8') as file:
+with open('data/Liga/code_snippets_small.txt', 'r', encoding='utf-8') as file:
     code_snippets_small = file.read()
+
 
 code_snippets_big2 = [snippet.replace('\n', ' ')
                       for snippet in code_snippets_big.strip().split('\n\n')]
@@ -33,7 +34,7 @@ code_snippets_small2 = [snippet.replace('\n', ' ')
 
 # export
 rows = zip(namen, ordnungszahl, altersklasse,
-           geschlecht, mannschafts_ID, widget_ID, code_snippets_big2, code_snippets_small2)
+           geschlecht, liganummer, widget_ID, code_snippets_big2, code_snippets_small2)
 
 # Write to a CSV file using a semicolon delimiter
 with open('output.csv', 'w', newline='', encoding='utf-8') as file:
