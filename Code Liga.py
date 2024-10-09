2dfc9319600a9e406003cae71cca723c03dacc51
AdminL3 <l-blu@outlook.de>
2024-10-09 11:29:10 +0200

    updated for liga widget
    from manschaft

--- Code Liga.py
+++ Code Liga.py
@@ -1,30 +1,30 @@
 with open('data/Liga/Widget_ID.txt', 'r', encoding='latin-1') as file:
     Widget_IDs = file.read().splitlines()
 
-with open('data/Liga/Mannschaft_ID.txt', 'r', encoding='latin-1') as file:
-    Mannschaft_IDs = file.read().splitlines()
+with open('data/Liga/liganummer.txt', 'r', encoding='latin-1') as file:
+    liganummern = file.read().splitlines()
 
 code_long = []
 # Generate the script code for each pair of Widget_ID and Mannschaft_ID
-for widget_id, manschaft_id in zip(Widget_IDs, Mannschaft_IDs):
+for widget_id, liganummer in zip(Widget_IDs, liganummern):
     script_code_long = f'''
-    <div id="{widget_id}"></div>
-    <script>
-        widget.mannschaftswidget('{widget_id}',
-            {{"iframeWidth":1200,"iframeHeight":600,"showRefreshButton":false,"titleColor":"FFFFFF","titleBgColor":"00CC00","tapColor":"FFFFFF","tapBgColor":"333333","colorMatchGroup":"666666","bgColorMatchGroup":"F0F0F0","colorMatchListItem":"000000","bgColorMatchListItem":"FFFFFF","showKuerzelInSpiele":false,"mannschaftsId":"{manschaft_id}"}} );
-    </script>
+    <div id="widget_{widget_id}"></div>
+        <script>
+                widget.ligawidget('widget_{widget_id}',
+                        {{"iframeWidth":1200,"iframeHeight":600,"showRefreshButton":true,"titleColor":"FFFFFF","titleBgColor":"00CC00","tapColor":"FFFFFF","tapBgColor":"333333","colorMatchGroup":"666666","bgColorMatchGroup":"F0F0F0","colorMatchListItem":"000000","bgColorMatchListItem":"FFFFFF","showKuerzelInSpiele":false,"ligaNr":"{liganummer}","vereinsId":"977","rangeDays":8,"activeTap":"tabelle","tapColorActive":"FFFFFF","tapBgColorActive":"000000","tapBorderColorActive":"00CC00","colorTableHeader":"666666","bgColorTableHeader":"F0F0F0","colorTableListItem":"000000","bgColorTableListItem":"FFFFFF","colorTableListItem2":"000000","bgColorTableListItem2":"FFFFFF","showKuerzelInTabelle":false}});
+        </script>
     '''
     code_long.append(script_code_long)
 
 code_short = []
 # Generate the script code for each pair of Widget_ID and Mannschaft_ID
-for widget_id, manschaft_id in zip(Widget_IDs, Mannschaft_IDs):
+for widget_id, liganummer in zip(Widget_IDs, liganummern):
     script_code_short = f'''
-    <div id="{widget_id}"></div>
-    <script>
-        widget.mannschaftswidget('{widget_id}',
-            {{"iframeWidth":600,"iframeHeight":700,"showRefreshButton":false,"titleColor":"FFFFFF","titleBgColor":"00CC00","tapColor":"FFFFFF","tapBgColor":"333333","colorMatchGroup":"666666","bgColorMatchGroup":"F0F0F0","colorMatchListItem":"000000","bgColorMatchListItem":"FFFFFF","showKuerzelInSpiele":false,"mannschaftsId":"{manschaft_id}"}} );
-    </script>
+    <div id="widget_{widget_id}"></div>
+        <script>
+                widget.ligawidget('widget_{widget_id}',
+                        {{"iframeWidth":600,"iframeHeight":600,"showRefreshButton":true,"titleColor":"FFFFFF","titleBgColor":"00CC00","tapColor":"FFFFFF","tapBgColor":"333333","colorMatchGroup":"666666","bgColorMatchGroup":"F0F0F0","colorMatchListItem":"000000","bgColorMatchListItem":"FFFFFF","showKuerzelInSpiele":false,"ligaNr":"{liganummer}","vereinsId":"977","rangeDays":8,"activeTap":"tabelle","tapColorActive":"FFFFFF","tapBgColorActive":"000000","tapBorderColorActive":"00CC00","colorTableHeader":"666666","bgColorTableHeader":"F0F0F0","colorTableListItem":"000000","bgColorTableListItem":"FFFFFF","colorTableListItem2":"000000","bgColorTableListItem2":"FFFFFF","showKuerzelInTabelle":false}});
+        </script>
     '''
     code_short.append(script_code_short)
 
