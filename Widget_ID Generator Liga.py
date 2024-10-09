fb057fc6e5be0cf8a9ff4242ea06ae031a28d35d
AdminL3 <l-blu@outlook.de>
2024-10-09 09:42:11 +0200

    duplicatedd this code for liga widget

---
+++ Widget_ID Generator Liga.py
@@ -0,0 +1,39 @@
+import re
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+with open('data/Liga/Liganummer.txt', 'r', encoding='latin-1') as file:
+    liganummer = file.read().splitlines()
+
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+options.add_argument("--disable-search-engine-choice-screen")
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+Widget_IDs = []
+
+for nummer in liganummer:
+    url = "https://www.basketball-bund.net/widget/widgets/#!/widgetgenerator/liga/" + \
+        str(nummer) + "/verein/977"
+    print(url)
+    driver.get(url)
+    div = driver.find_element(By.CLASS_NAME, "widget-code")
+    spans = div.find_elements(By.CSS_SELECTOR, "span")
+    text = spans[1].text
+    widget_id = re.findall(r'widget_(\d+)', text)
+    if widget_id:
+        # Append only the first (and likely only) match
+        Widget_IDs.append(widget_id[0])
+
+print(Widget_IDs)
+
+with open('data/Liga/Widget_ID.txt', 'w') as file:
+    file.writelines(f"{data}\n" for data in Widget_IDs)
+
+
+for i in range(4):
+    time.sleep(1)
+    print(4-i)
+driver.quit()
