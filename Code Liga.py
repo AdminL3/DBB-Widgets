5b1f577889ab98a0366cddea36f6b8cd771dfdd5
AdminL3 <l-blu@outlook.de>
2024-10-09 12:38:07 +0200

    indentation

--- Code Liga.py
+++ Code Liga.py
@@ -10,8 +10,8 @@ for widget_id, liganummer in zip(Widget_IDs, liganummern):
     script_code_long = f'''
     <div id="widget_{widget_id}"></div>
         <script>
-                widget.ligawidget('widget_{widget_id}',
-                        {{"iframeWidth":1200,"iframeHeight":600,"showRefreshButton":true,"titleColor":"FFFFFF","titleBgColor":"00CC00","tapColor":"FFFFFF","tapBgColor":"333333","colorMatchGroup":"666666","bgColorMatchGroup":"F0F0F0","colorMatchListItem":"000000","bgColorMatchListItem":"FFFFFF","showKuerzelInSpiele":false,"ligaNr":"{liganummer}","vereinsId":"977","rangeDays":8,"activeTap":"tabelle","tapColorActive":"FFFFFF","tapBgColorActive":"000000","tapBorderColorActive":"00CC00","colorTableHeader":"666666","bgColorTableHeader":"F0F0F0","colorTableListItem":"000000","bgColorTableListItem":"FFFFFF","colorTableListItem2":"000000","bgColorTableListItem2":"FFFFFF","showKuerzelInTabelle":false}});
+            widget.ligawidget('widget_{widget_id}',
+                    {{"iframeWidth":1200,"iframeHeight":600,"showRefreshButton":true,"titleColor":"FFFFFF","titleBgColor":"00CC00","tapColor":"FFFFFF","tapBgColor":"333333","colorMatchGroup":"666666","bgColorMatchGroup":"F0F0F0","colorMatchListItem":"000000","bgColorMatchListItem":"FFFFFF","showKuerzelInSpiele":false,"ligaNr":"{liganummer}","vereinsId":"977","rangeDays":8,"activeTap":"tabelle","tapColorActive":"FFFFFF","tapBgColorActive":"000000","tapBorderColorActive":"00CC00","colorTableHeader":"666666","bgColorTableHeader":"F0F0F0","colorTableListItem":"000000","bgColorTableListItem":"FFFFFF","colorTableListItem2":"000000","bgColorTableListItem2":"FFFFFF","showKuerzelInTabelle":false}});
         </script>
     '''
     code_long.append(script_code_long)
